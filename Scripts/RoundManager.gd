f106a0055638e6a6b6a28f370b92f89f2d05e926
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-08-24 11:31:53 -0400

    Updated Scripts. Created a rough draft of the workflow.

--- Scripts/RoundManager.gd
+++ Scripts/RoundManager.gd
@@ -1,13 +1,19 @@
 extends Node
 
+# This function marks the beginning of the round. Here you can setup whatever needs to be setup before players start taking turns.
 func start_round() -> void:
 	print("Start Round")
 
+# This function is called for the current player to play a card.
 func play_card() -> void:
 	print("Play Card")
 
-func check_bust() -> void:
+# This function will check if the player has busted or not
+func check_bust() -> bool:
+	var temp_busted = true
 	print("Check if player Busted")
+	return temp_busted
 
+# This function marks the end of the round. Here you can do whatever needs to be done after players have taken their turns or someone busted.
 func end_round() -> void:
 	print("End Round")
--- Scripts/TurnManager.gd
+++ Scripts/TurnManager.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var temp_players = ["Player 1", "Player 2", "Player 3", "Player 4"]
+var current_player_index: int = 0
+
+# This function is called at the start of a player's turn. Here you can setup whatever needs to be setup at the start of the players turn.
 func start_turn() -> void:
 	print("Start Turn")
 
@@ -8,7 +12,10 @@ func end_turn() -> void:
 
 func next_player() -> void:
 	print("Next Player")
+	current_player_index += 1
+	if current_player_index >= temp_players.size():
+		current_player_index = 0
 
-func get_current_player() -> void:
-	print("Get Current Player")
-
+func get_current_player() -> String:
+		print("Get Current Player")
+		return temp_players[current_player_index]
