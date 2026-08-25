65c8312a608c4a30eee7c9279c2605877b3488a3
Roo <clarathle@gmail.com>
2026-08-24 22:24:59 -0400

    [roo] making pile updates
    set up methods for things i thought might be needed, didnt hook it up to anything yet

---
+++ Scripts/Pile/PileRules.gd
@@ -0,0 +1,9 @@
+class_name PileRules
+extends Resource
+
+@export var bust_threshold: int = 10
+
+@export var min_card_value: int = 1
+@export var max_card_value: int = 5
+
+@export var legal_face_down_values: Array[int] = [1]
