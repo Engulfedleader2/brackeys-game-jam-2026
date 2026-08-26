c48c00695f10a246aa478527f19eae3d431c85aa
Justin Kim <naznsan@gmail.com>
2026-08-26 11:48:27 +1000

    add players to roundmanager

--- Scripts/RoundManager.gd
+++ Scripts/RoundManager.gd
@@ -1,25 +1,25 @@
 extends Node
 
-var hands: Array[Hand] = []
+var players: Array[Player] = []
 var shared_pile: SharedPile
 var deck: Deck
+var _current_player_index: int = 0
 
 
-func setup(p_hands: Array[Hand], p_shared_pile: SharedPile, p_deck: Deck) -> void:
-	hands = p_hands
+func setup(p_players: Array[Player], p_shared_pile: SharedPile, p_deck: Deck) -> void:
+	players = p_players
 	shared_pile = p_shared_pile
 	deck = p_deck
 
-	for i in hands.size():
-		hands[i].card_played.connect(_on_hand_card_played.bind(i))
+	for i in players.size():
+		players[i].card_played.connect(_on_player_card_played.bind(i))
 
-
-func _on_hand_card_played(instance: CardInstance, face_down: bool, owner_id: int) -> void:
-	shared_pile.play_card(instance, owner_id, face_down)
 
 # This function marks the beginning of the round. Here you can setup whatever needs to be setup before players start taking turns.
 func start_round() -> void:
-	print("Start Round")
+	_current_player_index = 0
+	players[_current_player_index].start_turn()
+
 
 # This function is called for the current player to play a card.
 func play_card() -> void:
@@ -34,3 +34,12 @@ func check_bust() -> bool:
 # This function marks the end of the round. Here you can do whatever needs to be done after players have taken their turns or someone busted.
 func end_round() -> void:
 	print("End Round")
+
+
+func _advance_turn() -> void:
+	_current_player_index = (_current_player_index + 1) % players.size()
+	players[_current_player_index].start_turn()
+
+
+func _on_player_card_played(instance: CardInstance, face_down: bool, owner_id: int) -> void:
+	shared_pile.play_card(instance, owner_id, face_down)
