e213d4f999f09e24c70507a24b6d5f51b20d28df
Israel <engulfedleader@gmail.com>
2026-08-26 08:46:45 -0400

    Add await to AIPlayer

--- Scripts/AIPlayer/AIPlayer.gd
+++ Scripts/AIPlayer/AIPlayer.gd
@@ -9,6 +9,7 @@ func start_turn() -> void:
 	var new_card := RoundManager.deck.draw_card()
 	if new_card != null:
 		hand.add_card(new_card)
+		await get_tree().process_frame
 
 	if not hand.cards.is_empty():
 		hand.cards[0].played.emit(false)
