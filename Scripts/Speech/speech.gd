22f6e5b3a08e23d2be7782d9bc859fced7f88a63
Kevin <k.horkaew2@gmail.com>
2026-08-26 21:15:49 -0400

    Squashed commit of the following:
    commit 7b939d9af51741d64a51c40bd3df59267fecc8be
    Author: Kevin <k.horkaew2@gmail.com>
    Date:   Wed Aug 26 00:41:27 2026 -0400
    
        Update bubble size
    
    commit f879a29fd9cdd893aa17a9f426cbb47805267781
    Author: Kevin <k.horkaew2@gmail.com>
    Date:   Wed Aug 26 00:14:25 2026 -0400
    
        Intial setup and test

---
+++ Scripts/Speech/speech.gd
@@ -0,0 +1,51 @@
+extends MarginContainer
+
+var index = 0
+var key = []
+var text1: String = "You’ll feel much better sitting between my teeth"
+var text2: String = "Good little Plushies"
+var text3: String = "Eeny. Meeny. Miny. Moe."
+var text4: String = "Your scent is….delectable"
+var text5: String = "[p][center]Why did the chicken cross the road…[/center][p][center]not to get away from me, I hope.[/center]"
+
+var final = 5
+var rng = RandomNumberGenerator.new()
+var random_range = randi_range(1,final)
+
+
+
+
+@onready var write_text = $MarginContainer/RichTextLabel
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomText()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	_text()
+
+
+func _text():
+	if index == 1:
+		write_text.text = text1
+	if index == 2:
+		write_text.text = text2
+	if index == 3:
+		write_text.text = text3
+	if index == 4:
+		write_text.text = text4
+	if index == 5:
+		write_text.text = text5
+
+
+func randomText():
+	var random_range = randi_range(1,final)
+	index = random_range
+
+
+func _on_test_speech_new_text_signal():
+	randomText()
