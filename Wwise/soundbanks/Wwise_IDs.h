78f3a209215f64eaa677f958524bc65bec430468
Quinne Houck <enbeatsu@gmail.com>
2026-08-27 14:54:36 -0700

    Added Wwise Integration Soundbank and assets
    Hooked up Wwise and the audio files; shouldn't have touched any of the actual stuff beyond adding the connection in. I intend to update my fork before I go in to add the audio manager and start testing the connection.

---
+++ Wwise/soundbanks/Wwise_IDs.h
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ALL_OFF = 2801173556U;
+        static const AkUniqueID BALL_OFF = 1274241660U;
+        static const AkUniqueID BALL_ON = 3426962870U;
+        static const AkUniqueID BEAR_OFF = 1342821817U;
+        static const AkUniqueID BEAR_ON = 2443688461U;
+        static const AkUniqueID CARROT_OFF = 559073066U;
+        static const AkUniqueID CARROT_ON = 1482611468U;
+        static const AkUniqueID CHICKEN_OFF = 3196734292U;
+        static const AkUniqueID CHICKEN_ON = 1177341774U;
+        static const AkUniqueID CREDITS = 2201105581U;
+        static const AkUniqueID DOG_OFF = 3677222191U;
+        static const AkUniqueID DOG_ON = 2138498875U;
+        static const AkUniqueID FOX_OFF = 3892601754U;
+        static const AkUniqueID FOX_ON = 3200743836U;
+        static const AkUniqueID GAME_OPEN = 1365208458U;
+        static const AkUniqueID LAMB_OFF = 1452776751U;
+        static const AkUniqueID LAMB_ON = 2836373307U;
+        static const AkUniqueID LOSS = 221232704U;
+        static const AkUniqueID SETTINGS = 2910527622U;
+        static const AkUniqueID SHOP = 251412225U;
+        static const AkUniqueID TABLE = 2048817515U;
+        static const AkUniqueID TITLE = 3705726509U;
+        static const AkUniqueID TRY_AGAIN = 308470013U;
+        static const AkUniqueID WIN = 979765101U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME
+        {
+            static const AkUniqueID GROUP = 702482391U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LOSS = 221232704U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SHOP = 251412225U;
+                static const AkUniqueID TABLE = 2048817515U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace GAME
+
+        namespace OPPONENT
+        {
+            static const AkUniqueID GROUP = 4158018714U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BALL = 1777984502U;
+                static const AkUniqueID BEAR = 1190179415U;
+                static const AkUniqueID CARROT = 2043204404U;
+                static const AkUniqueID CHICKEN = 4251222990U;
+                static const AkUniqueID DOG = 546651181U;
+                static const AkUniqueID FOX = 1083137540U;
+                static const AkUniqueID LAMB = 657744941U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace OPPONENT
+
+        namespace STAGE
+        {
+            static const AkUniqueID GROUP = 1063701865U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STAGE_01 = 344719509U;
+                static const AkUniqueID STAGE_02 = 344719510U;
+                static const AkUniqueID STAGE_03 = 344719511U;
+                static const AkUniqueID STAGE_04 = 344719504U;
+            } // namespace STATE
+        } // namespace STAGE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace OPPONENTS
+        {
+            static const AkUniqueID GROUP = 3227987485U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BALL = 1777984502U;
+                static const AkUniqueID BEAR = 1190179415U;
+                static const AkUniqueID CARROT = 2043204404U;
+                static const AkUniqueID CHICKEN = 4251222990U;
+                static const AkUniqueID DOG = 546651181U;
+                static const AkUniqueID FOX = 1083137540U;
+                static const AkUniqueID LAMB = 657744941U;
+            } // namespace SWITCH
+        } // namespace OPPONENTS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BALL = 1777984502U;
+        static const AkUniqueID BEAR = 1190179415U;
+        static const AkUniqueID CARROT = 2043204404U;
+        static const AkUniqueID CHICKEN = 4251222990U;
+        static const AkUniqueID CREDITS = 2201105581U;
+        static const AkUniqueID DOG = 546651181U;
+        static const AkUniqueID FOX = 1083137540U;
+        static const AkUniqueID LAMB = 657744941U;
+        static const AkUniqueID LOSS = 221232704U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID SETTINGS = 2910527622U;
+        static const AkUniqueID STRESS = 3840192365U;
+        static const AkUniqueID TABLE_STAGE = 536018156U;
+        static const AkUniqueID TITLE = 3705726509U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GAME = 702482391U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID STG = 628126519U;
+        static const AkUniqueID VOX = 543694036U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
