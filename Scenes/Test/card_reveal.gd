c2f10c7ab32cf72c3c508deacdc4a90860c9320d
Kevin <k.horkaew2@gmail.com>
2026-08-27 23:29:40 -0400

    Update card reveal

--- Scenes/Test/card_reveal.gd
+++ Scenes/Test/card_reveal.gd
@@ -8,7 +8,17 @@ const UNO_3 = preload("uid://iy8ojnt3ionl")
 const UNO_4 = preload("uid://coxv8co3cviey")
 const UNO_5 = preload("uid://k7y4wrytuemm")
 
+
+const CARROT_BLUFF = preload("uid://davd8ckv0fahp")
+const CARROT_CALL = preload("uid://objwwwag7mg0")
+const LAMB_CHOP_BLUFF = preload("uid://cjjdvkur7ysud")
+const LAMB_CHOP_CALL = preload("uid://bc53lxyq5d2tc")
+
+
+
 @onready var face_card = $Sprite2D2
+@onready var accused = $Accused
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,8 +26,10 @@ func _ready():
 	$AnimationPlayer.play("Shake")
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	_whoIsit()
 	if index == 1:
 		face_card.texture = UNO_1
 	if index == 2:
@@ -28,3 +40,10 @@ func _process(_delta):
 		face_card.texture = UNO_4
 	if index == 5:
 		face_card.texture = UNO_5
+
+
+func _whoIsit():
+	if index == 1:
+		accused.texture = CARROT_BLUFF
+	if index == 3:
+		accused.texture  = LAMB_CHOP_BLUFF
