eaa1830813c2f6b2f84bca9b422e860430c20b87
Israel <engulfedleader@gmail.com>
2026-08-28 10:48:33 -0400

    Fix rounds and win/loss condition (#18)

--- Scripts/Pile/PileRules.gd
+++ Scripts/Pile/PileRules.gd
@@ -6,4 +6,4 @@ extends Resource
 @export var min_card_value: int = 1
 @export var max_card_value: int = 5
 
-@export var legal_face_down_values: Array[int] = [1]
+@export var legal_face_down_values: Array[int] = [1, 5]
