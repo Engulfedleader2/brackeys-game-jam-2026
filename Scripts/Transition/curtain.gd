bc5a7d49acdad0c021b0739685b16bbc9a58e57e
Kevin <k.horkaew2@gmail.com>
2026-08-29 08:01:21 -0400

    Update Credits

--- Scripts/Transition/curtain.gd
+++ Scripts/Transition/curtain.gd
@@ -2,13 +2,18 @@ extends CanvasLayer
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
-
+var can_play = true
 
 func _reveal():
-	animation.play("Reveal")
+	if can_play == true:
+		animation.play("Reveal")
+	else:
+		$Control.position = Vector2(0,-1000)
 
 func _on_main_menu_start_game_signal() -> void:
+	can_play = true
 	show()
+	$Control.position = Vector2(0,-1271)
 	animation.play_backwards("Reveal")
 	await animation.animation_finished
 	await get_tree().create_timer(1).timeout
