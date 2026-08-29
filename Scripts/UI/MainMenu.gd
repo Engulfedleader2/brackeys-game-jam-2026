fe891f1a0b57160fdec43de014c01024cff5ef9d
Kevin <k.horkaew2@gmail.com>
2026-08-28 20:56:57 -0400

    Alt title/game button

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -10,24 +10,37 @@ extends Control
 func _ready() -> void:
 	await get_tree().create_timer(2).timeout
 	Curtain._reveal()
-	start_button.pressed.connect(_on_start_button_pressed)
-	settings_button.pressed.connect(_on_settings_button_pressed)
-	credits_button.pressed.connect(_on_credits_button_pressed)
-	quit_button.pressed.connect(_on_quit_button_pressed)
+	#start_button.pressed.connect(_on_start_button_pressed)
+	#settings_button.pressed.connect(_on_settings_button_pressed)
+	#credits_button.pressed.connect(_on_credits_button_pressed)
+	#quit_button.pressed.connect(_on_quit_button_pressed)
 
 
-func _on_start_button_pressed() -> void:
-	Curtain._on_main_menu_start_game_signal()
+#func _on_start_button_pressed() -> void:
+	#Curtain._on_main_menu_start_game_signal()
 	#SceneManager.go_to_game()
 
 
-func _on_settings_button_pressed() -> void:
+#func _on_settings_button_pressed() -> void:
+	#SceneManager.go_to_settings_menu()
+#
+#
+#func _on_credits_button_pressed() -> void:
+	#SceneManager.go_to_credits_menu()
+#
+#
+#func _on_quit_button_pressed() -> void:
+	#SceneManager.quit_game()
+
+
+
+func _on_setting_button_pressed():
 	SceneManager.go_to_settings_menu()
 
 
-func _on_credits_button_pressed() -> void:
+func _on_credit_button_pressed():
 	SceneManager.go_to_credits_menu()
 
 
-func _on_quit_button_pressed() -> void:
-	SceneManager.quit_game()
+func _on_start_button_pressed():
+	Curtain._on_main_menu_start_game_signal()
