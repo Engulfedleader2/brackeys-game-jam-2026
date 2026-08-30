777b38d78b89430c523c421396913815a1ff901b
ClaraLe <clarathle@gmail.com>
2026-08-30 12:52:19 -0400

    [roo] added so pile counts bluffed amounts and reveals last declared … (#34)
    * [roo] added so pile counts bluffed amounts and reveals last declared value to player
    
    * fixed squeakybear

--- Scripts/Pile/PileEntry.gd
+++ Scripts/Pile/PileEntry.gd
@@ -9,11 +9,12 @@ var revealed: bool = false
 var declared_value: int = -1 # For face-down cards: what player declared (1 or 5)
 
 
-func _init(c_instance: CardInstance, c_owner_id: int, c_face_down: bool, c_play_index: int) -> void:
+func _init(c_instance: CardInstance, c_owner_id: int, c_face_down: bool, c_play_index: int, c_declared: int) -> void:
 	instance = c_instance
 	face_down = c_face_down
 	owner_id = c_owner_id
 	play_index = c_play_index
+	declared_value = c_declared
 
 func actual_value() -> int:
 	return instance.resource.value
@@ -25,7 +26,8 @@ func base_value() -> int:
 	return instance.base_value()
 
 func effective_value() -> int:
-	return instance.effective_value()
+	var value := declared_value if face_down else base_value()
+	return -value if instance.negated else value
 
 func is_declared_lie() -> bool:
 	if not face_down or declared_value == -1:
