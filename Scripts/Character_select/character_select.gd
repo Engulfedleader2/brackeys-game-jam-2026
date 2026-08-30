70ffbc0e6a20ae0156d49bde6c8ce69bfc890792
Kevin <k.horkaew2@gmail.com>
2026-08-30 01:31:17 -0400

    Update Character Select

--- Scripts/Character_select/character_select.gd
+++ Scripts/Character_select/character_select.gd
@@ -7,16 +7,26 @@ extends Node2D
 @onready var fox: Sprite2D = $Control/Fox
 @onready var lamb: Sprite2D = $Control/Lamb
 
-@onready var bear_button: Button = $Bear_Button
-@onready var fox_button: Button = $Fox_Button
-@onready var dachs_button: Button = $Dachs_Button
-@onready var broc_button: Button = $Broc_Button
-@onready var lamb_button: Button = $Lamb_Button
-@onready var chicken_button: Button = $Chicken_Button
-@onready var carrot_button: Button = $Carrot_Button
+
+@onready var all_buttons: Control = $Buttons
+@onready var bear_button: Button = $Buttons/Bear_Button
+@onready var fox_button: Button = $Buttons/Fox_Button
+@onready var dachs_button: Button = $Buttons/Dachs_Button
+@onready var broc_button: Button = $Buttons/Broc_Button
+@onready var lamb_button: Button = $Buttons/Lamb_Button
+@onready var chicken_button: Button = $Buttons/Chicken_Button
+@onready var carrot_button: Button = $Buttons/Carrot_Button
 
 
+var bear_select = false
+var fox_select = false
+var dachs_select = false
+var broc_select = false
+var lamb_select = false
+var chicken_select = false
+var carrot_select = false
 
+var can_select = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,6 +34,9 @@ func _ready() -> void:
 	Curtain._reveal()
 	halfDim()
 
+func _process(_delta: float) -> void:
+	if can_select == false:
+		all_buttons.hide()
 
 
 func halfDim():
@@ -35,71 +48,111 @@ func halfDim():
 	fox.modulate.a = 0
 	lamb.modulate.a = 0
 
+#region Tween property for characters
 func _on_bear_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(bear,"modulate:a",1,.2)
 
 
 func _on_bear_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property(bear,"modulate:a",0,.2)
+	if bear_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(bear,"modulate:a",0,.2)
 
 
 func _on_fox_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(fox,"modulate:a",1,.2)
 
 
 func _on_fox_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if fox_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(fox,"modulate:a",0,.2)
 
 
 func _on_dachs_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(dachs,"modulate:a",1,.2)
 
 
 func _on_dachs_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if dachs_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(dachs,"modulate:a",0,.2)
 
 
 func _on_broc_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(broc,"modulate:a",1,.2)
 
 
 func _on_broc_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if broc_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(broc,"modulate:a",0,.2)
 
 
 func _on_lamb_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(lamb,"modulate:a",1,.2)
 
 
 func _on_lamb_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if lamb_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(lamb,"modulate:a",0,.2)
 
 
 func _on_chicken_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(chicken,"modulate:a",1,.2)
 
 
 func _on_chicken_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if chicken_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(chicken,"modulate:a",0,.2)
 
 
 func _on_carrot_button_mouse_entered() -> void:
 	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	tween.tween_property(carrot,"modulate:a",1,.2)
 
 
 func _on_carrot_button_mouse_exited() -> void:
-	var tween = get_tree().create_tween()
-	pass # Replace with function body.
+	if carrot_select == false:
+		var tween = get_tree().create_tween()
+		tween.tween_property(carrot,"modulate:a",0,.2)
+#endregion
+
+
+#region Button pressed
+func _on_bear_button_pressed() -> void:
+	bear_select = true
+	
+
+
+func _on_fox_button_pressed() -> void:
+	fox_select = true
+
+
+func _on_dachs_button_pressed() -> void:
+	dachs_select = true
+
+
+func _on_broc_button_pressed() -> void:
+	broc_select = true
+
+
+func _on_lamb_button_pressed() -> void:
+	lamb_select = true
+
+
+func _on_chicken_button_pressed() -> void:
+	chicken_select = true
+
+
+func _on_carrot_button_pressed() -> void:
+	carrot_select = true
+#endregion
