774ecd809c80a186f1adcf0ab1572b4a4ee5f6ea
Kevin <k.horkaew2@gmail.com>
2026-08-30 03:35:35 -0400

    Rework bluff animation

--- Scenes/Test/card_reveal.gd
+++ Scenes/Test/card_reveal.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-var index = 3
+var index = 1
 
 const UNO_1 = preload("uid://d0vm0wuh17gbd")
 const UNO_2 = preload("uid://noo2kwaa0l2w")
@@ -9,11 +9,16 @@ const UNO_4 = preload("uid://coxv8co3cviey")
 const UNO_5 = preload("uid://k7y4wrytuemm")
 
 
-const CARROT_BLUFF = preload("uid://davd8ckv0fahp")
-const CARROT_CALL = preload("uid://objwwwag7mg0")
-const LAMB_CHOP_BLUFF = preload("uid://cjjdvkur7ysud")
-const LAMB_CHOP_CALL = preload("uid://bc53lxyq5d2tc")
+const BEAR_BLUFF = preload("uid://dh0x006cv43vr")
+const BROCCOLI_BLUFF = preload("uid://c3ndo80teqmel")
+const CARROT_BLUFF = preload("uid://bdc030l0rluj3")
+const DACHSHUND_BLUFF = preload("uid://86kobshebo2")
+const FOX_BLUFF = preload("uid://cgae8wr1esp66")
+const LAMB_CHOP_BLUFF = preload("uid://bsyhwvq5xti2b")
+const SOFT_CHICKEN_BLUFF = preload("uid://4m5bwwydby3w")
 
+
+var current_character
 
 
 @onready var face_card = $Sprite2D2
@@ -44,16 +49,46 @@ func _process(_delta):
 
 
 func _whoIsit():
-	if index == 1:
+	if CharacterSelection.chosen_toy_class == SqueakyBear:
+		accused.texture = BEAR_BLUFF
+	elif CharacterSelection.chosen_toy_class == FoxPlush:
+		accused.texture = FOX_BLUFF
+	elif CharacterSelection.chosen_toy_class == Broccoli:
+		accused.texture = BROCCOLI_BLUFF
+	elif CharacterSelection.chosen_toy_class == ClothCarrot:
 		accused.texture = CARROT_BLUFF
-	if index == 3:
-		accused.texture  = LAMB_CHOP_BLUFF
+	elif CharacterSelection.chosen_toy_class == DachshundDogPlush:
+		accused.texture = DACHSHUND_BLUFF
+	elif CharacterSelection.chosen_toy_class == LambChop:
+		accused.texture = LambChop
+	elif CharacterSelection.chosen_toy_class == SoftChicken:
+		accused.texture = SOFT_CHICKEN_BLUFF
+
 
 
 func _on_call_bluff_prompt_closed(did_call):
 	if did_call == true:
+		_whoIsit()
+		get_tree().paused = true
 		$".".show()
 		$AnimationPlayer.play("Shake")
 		$Accused_AnimationPlayer.play("zoom")
 		await get_tree().create_timer(5).timeout
 		$".".hide()
+		get_tree().paused = false
+
+
+
+
+
+func _on_shared_pile_value_signal(entry) -> void:
+	if entry == 1:
+		index = 1
+	if entry == 2:
+		index = 2
+	if entry == 3:
+		index = 3
+	if entry == 4:
+		index = 4
+	if entry == 5:
+		index = 5
