a337faf500094b427e68c7d628a31f4bda8d2464
Roo <clarathle@gmail.com>
2026-08-30 21:11:55 -0400

    [roo] fixing broccoli player being seen as a chicken
    also added a (you) to make it obvious when using the hall pass that your skipping your own turn

--- Scripts/AIPlayer/Toys/Broccoli.gd
+++ Scripts/AIPlayer/Toys/Broccoli.gd
@@ -6,7 +6,7 @@ func _init() -> void:
 	display_name = "Broccoli"
 	description = "Call when top card holds a 1 or 5."
 	# No dedicated art exists for Broccoli yet - reusing Soft Chicken's as a placeholder.
-	icon = preload("res://AssetDump/Spooky/Soft_Chicken.png")
+	icon = preload("res://AssetDump/Characters/Broccoli.png")
 
 func should_call(pile_state: Dictionary) -> bool:
 	var top_value = pile_state.get("top_value", -1)
