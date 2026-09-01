23398c731625d6961fa25829d3e64bbebdbd9022
Kevin <98916435+maxometer10@users.noreply.github.com>
2026-08-31 23:14:49 -0400

    Update how to play back button (#40)
    * Update how to play back button
    
    * Update speech.gd
    
    * Update speech.gd
    
    Change frequency of bubble coming on screen

--- Scripts/Speech/speech.gd
+++ Scripts/Speech/speech.gd
@@ -7,8 +7,11 @@ var text2: String = "Good little Plushies"
 var text3: String = "Eeny. Meeny. Miny. Moe."
 var text4: String = "Your scent is….delectable"
 var text5: String = "[p][center]Why did the chicken cross the road…[/center][p][center]not to get away from me, I hope.[/center]"
+var text6: String = "Mine, mine, mine. Such soft fur you got"
 
-var final = 5
+
+
+var final = 6
 var rng = RandomNumberGenerator.new()
 var random_range = randi_range(1,final)
 
@@ -41,6 +44,8 @@ func _text():
 		write_text.text = text4
 	if index == 5:
 		write_text.text = text5
+	if index == 6:
+		write_text.text = text6
 
 
 func randomText():
@@ -53,9 +58,13 @@ func _on_test_speech_new_text_signal():
 
 
 func _on_shared_pile_speech_signal() -> void:
-	show()
-	get_tree().create_tween().tween_property(self,"modulate:a", 1, .1)
-	randomText()
+	var result = randi_range(1,300)
+	if result % 7: 
+		show()
+		get_tree().create_tween().tween_property(self,"modulate:a", 1, .1)
+		randomText()
+	else:
+		pass
 
 
 func _leave():
